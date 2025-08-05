956aedcebac17c34965e4979254585d8dbb8265d
Lucky Raj <kumari.lucky.raj.cse.2023@tint.edu.in>
2025-08-05 13:08:46 +0530

    done

---
+++ app/api/debug-db/page.js
@@ -0,0 +1,10 @@
+// app/api/debug-db/route.js
+import connectDb from "@/db/connectDb";
+import mongoose from "mongoose";
+
+export async function GET() {
+  await connectDb();
+  return new Response(`Connected to DB: ${mongoose.connection.name}`, {
+    status: 200,
+  });
+}
