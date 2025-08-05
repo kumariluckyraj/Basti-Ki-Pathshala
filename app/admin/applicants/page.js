a49d35b899b4355ab29f7cde05eff7cf39bb2601
Lucky Raj <kumari.lucky.raj.cse.2023@tint.edu.in>
2025-08-05 14:27:12 +0530

    fix done

--- app/admin/applicants/page.js
+++ app/admin/applicants/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';//
 
 const AdminApplicantsPage = () => {
   const [applicants, setApplicants] = useState([]);
